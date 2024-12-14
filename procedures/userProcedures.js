167a675e87a229feaf8ce007c9ca3e8cfee79d3f
tahayk3 <tahayk3@gmail.com>
2024-12-13 19:08:25 -0600

    utilizando procedimientos para eliminar usuario y cambiar usuario

--- procedures/userProcedures.js
+++ procedures/userProcedures.js
@@ -5,9 +5,14 @@ async function getUsers() {
   return results;
 }
 
-async function getUser(id_usuario){
-  const [results] = await sequelize.query("EXEC ConsultarUsuarioPorID @id_usuario = id_usuario",{replacements:{id_usuario}});
-  return results;
+async function getUser(id_usuario) {
+  const [results] = await sequelize.query(
+    `EXEC ConsultarUsuarioPorID
+    @id_usuario = :id_usuario
+    `,  {
+      replacements: {id_usuario}
+    });
+    return results;
 }
 
 async function createUser(correo, nombre_completo, password, telefono, fecha_nacimiento, fecha_creacion, id_rol, id_cliente, id_estado) {
@@ -28,9 +33,36 @@ async function createUser(correo, nombre_completo, password, telefono, fecha_nac
   return results;
 }
 
+async function desactiveUser(id_usuario){
+  const [results] = await sequelize.query(
+    `EXEC EliminarUsuario
+    @id_usuario = :id_usuario
+    `,  {
+      replacements: {id_usuario}
+    });
+    return results;
+}
 
+async function changeUser(id_usuario, correo, nombre_completo, password, telefono, fecha_nacimiento, id_rol, id_cliente, id_estado){
+  const [results] = await sequelize.query(
+    `EXEC ActualizarUsuario
+    @id_usuario = :id_usuario, 
+    @correo = :correo,
+    @nombre_completo = :nombre_completo,
+    @password = :password,
+    @telefono = :telefono,
+    @fecha_nacimiento = :fecha_nacimiento,
+    @id_rol = :id_rol,
+    @id_cliente = :id_cliente,
+    @id_estado = :id_estado
+    `, {
+    replacements: {id_usuario, correo, nombre_completo,  password, telefono, fecha_nacimiento, id_rol, id_cliente, id_estado  },
+  });
+  return results;
+}
 
-module.exports = { getUsers, createUser, getUser };
+ 
+module.exports = { getUsers, createUser, getUser, desactiveUser, changeUser };
 
 
 
